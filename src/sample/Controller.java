423b72a73d6e94c9739ddd8903ffca29abc2c34c
Gillouz21 <gil210904@gmail.com>
2023-01-08 19:26:58 +0200

    first version

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+
+public class Controller {
+
+
+
+
+
+
+}
