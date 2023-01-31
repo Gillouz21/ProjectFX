92c305f9f31c6746b7e95bb348a46e1b01c2770e
Gillouz21 <gil210904@gmail.com>
2023-01-31 12:15:08 +0200

    almost complete map

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,45 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import javax.swing.text.html.ImageView;
 
 public class Controller {
 
+    @FXML
+    private Rectangle Hus2Lub;
+
+    @FXML
+    private Rectangle Hus2Lub1;
+
+    @FXML
+    private Rectangle Hus2Lub2;
+
+    @FXML
+    private Pane Hus2LubP;
+
+    public void example()
+    {
+        Hus2LubP.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Hus2Lub.setFill(Color.RED);
+                Hus2Lub1.setFill(Color.RED);
+                Hus2Lub2.setFill(Color.RED);
+            }
+        });
 
 
 
 
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +15,20 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+
+        Scene s = new Scene(root);
+
+        String str = "#Route1";
+
+        Pane p = (Pane)s.lookup(str);
+
+        p.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("clicked ");
+            }
+        });
+        primaryStage.setScene(s);
         primaryStage.show();
     }
 
